60fef6805dbf311d821b715e98819a29a6cd6ac2
TheGreatLy <2411082040@qq.com>
2019-06-22 09:32:33 +0800

    media

--- js/main.js
+++ js/main.js
@@ -52,7 +52,6 @@
             times();
         },2000);
     }
-
     //定时器轮播
     function times() {
         if (time===3){
@@ -60,19 +59,17 @@
         }else{
             time++;
         }
-        btns[time].classList.add("active");
-        btns[s].classList.remove("active");
+        btns.eq(time).addClass("active");
+        btns.eq(s).removeClass("active");
         s = time;
-        banner[0].style.marginLeft = `${-100*time}%`;
+        banner.css({marginLeft:`${-100*time}%`})
     }
 
     anim();
 
     //鼠标移入暂停轮播
     swiper.on('mouseenter',()=>{
-        //console.log(timer);
         clearInterval(timer);
-        //console.log(timer);
     });
 
     //鼠标移出开启轮播
@@ -81,37 +78,37 @@
     });
 
     //右按钮
-    lr[1].onclick = function () {
+    lr.eq(1).click (function () {
+        console.log("r");
         times();
-    };
+    });
 
     //左按钮
-    lr[0].onclick = function () {
+    lr.eq(0).click(function () {
         if (time===0){
             time = 3;
         }else{
             time--;
         }
-        btns[time].classList.add("active");
-        btns[s].classList.remove("active");
+        btns.eq(time).addClass("active");
+        btns.eq(s).removeClass("active");
         s = time;
-        banner[0].style.marginLeft = `${-100*time}%`;
-    };
+        banner.css({marginLeft:`${-100*time}%`})
+    });
 
     //小按钮切换动画
     function change(index) {
-        btns[index].classList.add("active");
-        btns[time].classList.remove("active");
+        btns.eq(index).addClass("active");
+        btns.eq(time).removeClass("active");
         s = index;
         time = index;
-        banner[0].style.marginLeft = `${-100*index}%`;
+        banner.css({marginLeft:`${-100*index}%`})
     }
 
     //监听小按钮点击事件
-    [...btns].forEach((ele,index)=>{
-        ele.onclick = function(){
-            change(index);
-        }
+    let btn = $('.btn');
+    btn.on('click','li',function () {
+        change($(this).index());
     })
 
 })();
